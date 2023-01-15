4ef732647d9671a133d59ad46c54a2cc75d0d274
Evgen <timeinvest27@gmail.com>
2023-01-15 22:29:44 +0300

    Add project files.

---
+++ Contest1782ProblemB/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    static void Main()
+    {
+        var countTests = int.Parse(Console.ReadLine());
+
+        for (int t = 0; t < countTests; t++)
+        {
+            var peopleCount = int.Parse(Console.ReadLine());
+            var dreams = Console.ReadLine().Split(" ");
+            var dreamsArray = new int[peopleCount];
+
+            for (int i = 0; i < dreams.Length; i++)
+            {
+                dreamsArray[i] = int.Parse(dreams[i]);
+            }
+
+            var result = 0;
+
+            var binaryString = new string('1', peopleCount);
+            var variantCount = Convert.ToInt32(binaryString, 2);
+
+
+            for (int i = 0; i <= variantCount; i++)
+            {
+                var currentResult = true;
+                string binary = new string(Convert.ToString(i, 2).Reverse().ToArray());
+                var goCount = binary.Count(_ => _ == '1');
+
+                for (int j = 0; j < peopleCount; j++)
+                {
+                    if (binary.Length < j + 1 || binary[j] == '0') //no going
+                    {
+                        if (goCount >= dreamsArray[j])
+                        {
+                            currentResult = false;
+                            break;
+                        }
+                    }
+                    else //going
+                    {
+                        if (goCount < dreamsArray[j] + 1)
+                        {
+                            currentResult = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (currentResult)
+                {
+                    result++;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Сontest1782ЗroblemA/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        var countTests = int.Parse(Console.ReadLine());
+
+        for (int t = 0; t < countTests; t++)
+        {
+            var roomSize = Console.ReadLine().Split(" ");
+            var coordinats = Console.ReadLine().Split(" ");
+
+            var w = int.Parse(roomSize[0]);
+            var d = int.Parse(roomSize[1]);
+            var h = int.Parse(roomSize[2]);
+
+            var a = int.Parse(coordinats[0]);
+            var b = int.Parse(coordinats[1]);
+            var f = int.Parse(coordinats[2]);
+            var g = int.Parse(coordinats[3]);
+
+            var result = h;
+
+            int[] array = new int[4];
+            array[0] = b + g + Math.Abs(a - f); //down
+            array[1] = (d - b) + (d - g) + Math.Abs(a - f); //up
+
+            array[2] = a + f + Math.Abs(b - g); //left
+            array[3] = (w - a) + (w - f) + Math.Abs(b - g); //right
+
+            int tmpMin = int.MaxValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (tmpMin > array[i])
+                {
+                    tmpMin = array[i];
+                }
+            }
+
+            result += tmpMin;
+
+            Console.WriteLine(result);
+        }
+    }
+}
